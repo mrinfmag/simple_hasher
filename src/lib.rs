ce59952a74827986b1953833d1d4cbc7f9e58b08
Mattia Rossini <mattia.rossini@en-ergo.it>
2020-04-30 15:35:58 +0200

    added raw data hashing functions

--- src/lib.rs
+++ src/lib.rs
@@ -2,37 +2,63 @@
 pub mod simple_hasher {
     use sha2::Digest;
 
-    fn hex_string(input: &[u8]) -> String {
+    /// Performs the conversion from `u8` slice to `String`
+    pub fn hex_string(input: &[u8]) -> String {
         input
             .as_ref()
             .iter()
-            .map(|b| format!("{:x}", b))
+            .map(|byte| format!("{:x}", byte))
             .collect()
     }
 
     /// Generic hashing function
-    fn sha<T>(mut hasher: T, str: String) -> String where T: Digest {
+    fn sha<T>(mut hasher: T, data: &[u8]) -> &[u8] where T: Digest {
         hasher.input(str.as_bytes());
-        hex_string(&hasher.result())
+//        hex_string(&hasher.result())
+        &hasher.result()
     }
 
-    /// SHA-224 hashing function
-    pub fn sha224(s: String) -> String {
-        sha(sha2::Sha224::new(), s)
+    /* Raw data hashing functions */
+
+    /// SHA-224 raw data hashing function
+    pub fn sha224(s: &[u8]) -> &[u8] {
+        sha(sha2::Sha224::new(), d)
     }
 
-    /// SHA-256 hashing function
-    pub fn sha256(s: String) -> String {
-        sha(sha2::Sha256::new(), s)
+    /// SHA-256 raw data hashing function
+    pub fn sha256(s: &[u8]) -> &[u8] {
+        sha(sha2::Sha256::new(), d)
     }
 
-    /// SHA-384 hashing function
-    pub fn sha384(s: String) -> String {
-        sha(sha2::Sha384::new(), s)
+    /// SHA-384 raw data hashing function
+    pub fn sha384(s: &[u8]) -> &[u8] {
+        sha(sha2::Sha384::new(), d)
     }
 
-    /// SHA-512 hashing function
-    pub fn sha512(s: String) -> String {
-        sha(sha2::Sha512::new(), s)
+    /// SHA-512 raw data hashing function
+    pub fn sha512(s: &[u8]) -> &[u8] {
+        sha(sha2::Sha512::new(), d)
+    }
+
+    /* String hashing functions */
+
+    /// SHA-224 string hashing function
+    pub fn string_sha224(s: String) -> &[u8] {
+        sha224(s.as_bytes())
+    }
+
+    /// SHA-256 string hashing function
+    pub fn string_sha256(s: String) -> &[u8] {
+        sha256(s.as_bytes())
+    }
+
+    /// SHA-384 string hashing function
+    pub fn string_sha384(s: String) -> &[u8] {
+        sha384(s.as_bytes())
+    }
+
+    /// SHA-512 string hashing function
+    pub fn string_sha512(s: String) -> &[u8] {
+        sha512(s.as_bytes())
     }
 }
