530507876fc1a2a2cd6f53f1e3b3c2870cb2b7c0
Mattia Rossini <mattia.rossini@en-ergo.it>
2020-04-30 15:31:41 +0200

    fixed typo

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+/// Simple Hasher module
 pub mod simple_hasher {
     use sha2::Digest;
 
@@ -9,25 +10,29 @@ pub mod simple_hasher {
             .collect()
     }
 
-    fn sha<T>(mut hasher: T, s: String) -> String where T: Digest {
-        hasher.input(s.as_bytes());
-
+    /// Generic hashing function
+    fn sha<T>(mut hasher: T, str: String) -> String where T: Digest {
+        hasher.input(str.as_bytes());
         hex_string(&hasher.result())
     }
 
+    /// SHA-224 hashing function
     pub fn sha224(s: String) -> String {
         sha(sha2::Sha224::new(), s)
     }
 
+    /// SHA-256 hashing function
     pub fn sha256(s: String) -> String {
         sha(sha2::Sha256::new(), s)
     }
 
+    /// SHA-384 hashing function
     pub fn sha384(s: String) -> String {
         sha(sha2::Sha384::new(), s)
     }
 
-    pub fn sha5124(s: String) -> String {
+    /// SHA-512 hashing function
+    pub fn sha512(s: String) -> String {
         sha(sha2::Sha512::new(), s)
     }
 }
